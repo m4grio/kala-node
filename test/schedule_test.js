974fe53af344f56b18f2d13a6af078efb4e11793
Mario Álvarez <ahoy@m4grio.me>
2017-10-15 21:51:50 +0800

    WIP: Add schedule string generator and tests

---
+++ test/schedule_test.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const Schedule = require('..').Schedule
+    , assert = require('assert')
+;
+
+describe('Schedule', () => {
+    it('should accept either three params or an object', () => {
+        var date = new Date();
+        var times = Math.random();
+        var interval = 'P1DT1M';
+
+        var withThreePArams = new Schedule(times, date, interval);
+        var withObject = new Schedule({times: times, start: date, interval: interval});
+
+        assert.strictEqual(typeof withThreePArams, 'object');
+        assert.equal(withThreePArams.toString(), withObject.toString());
+    });
+
+    it('should accept null for the `times` param', () => {
+        assert.doesNotThrow(() => {
+            new Schedule(null, new Date(), 'P1').toString();
+        });
+    });
+});
