46fd7f54791d8a6361c459d6db3386d6ce5f858b
Mario Álvarez <ahoy@m4grio.me>
2017-10-16 00:31:07 +0800

    WIP: Add ability to retrieve a job

--- test/client_test.js
+++ test/client_test.js
@@ -17,4 +17,25 @@ describe('Kala Client basic operations', () => {
             done();
         });
     });
+
+    it('retrieves a job', (done) => {
+        new Promise((resolve, reject) => {
+            let start = new Date();
+            start.setDate(start.getHours() + 24);
+            kala.createJob({
+                name: 'stuff',
+                command: '/bin/true',
+                schedule: new Kala.Schedule(null, start, 'PT15S').toString(),
+            }, (_, res) => {
+                resolve(res.body);
+            });
+        })
+        .then(job => {
+            kala.getJob(job.id, (err, res) => {
+                assert.equal(err, undefined);
+                assert.strictEqual(job.id, res.body.job.id);
+                done();
+            });
+        });
+    });
 });
